7b922e875bfd117d2af8a1dff005ff7f0eeff46f
Brian Vu <vubaonam0401@gmail.com>
2023-06-28 15:50:46 -0400

    styling the page

--- script.js
+++ script.js
@@ -1,40 +1,49 @@
-let speechArray = [
-  "Welcome to my software engineering portfolio.",
+// let speechArray = [
+//   "Welcome to my software engineering portfolio.",
 
-  "I specialize in building efficient, scalable apps.",
+//   "I specialize in building efficient, scalable apps.",
 
-  "Solving complex problems with elegant code solutions.",
+//   "Solving complex problems with elegant code solutions.",
 
-  "My focus is on creating user-centric software.",
+//   "My focus is on creating user-centric software.",
 
-  "I develop innovative solutions for modern problems.",
+//   "I develop innovative solutions for modern problems.",
 
-  "Crafting code that is both functional and beautiful.",
+//   "Crafting code that is both functional and beautiful.",
 
-  "I'm passionate about building scalable applications.",
+//   "I'm passionate about building scalable applications.",
 
-  "My expertise covers both frontend and backend.",
+//   "My expertise covers both frontend and backend.",
 
-  "I love pushing the boundaries of what's possible.",
+//   "I love pushing the boundaries of what's possible.",
 
-  "Every project is built with precision.",
+//   "Every project is built with precision.",
 
-  "My goal is to deliver exceptional results.",
+//   "My goal is to deliver exceptional results.",
 
-  "Hope you enjoy exploring my portfolio.",
-];
-document.getElementById("text_value").innerHTML = "Hi there!";
+//   "Hope you enjoy exploring my portfolio.",
+// ];
+// document.getElementById("text_value").innerHTML = "Hi there!";
 
-window.onload = function () {
-  for (let i = 0; i < speechArray.length; i++) {
-    window.setTimeout(updateText, 500 + i * 3000);
-    function updateText() {
-      document.getElementById("text_value").style.opacity = "0";
-      window.setTimeout(() => {
-        document.getElementById("text_value").innerHTML = speechArray[i];
-        document.getElementById("text_value").style.opacity = "1";
-      }, 1000);
-    }
-  }
-};
+// window.onload = function () {
+//   for (let i = 0; i < speechArray.length; i++) {
+//     window.setTimeout(updateText, 500 + i * 3000);
+//     function updateText() {
+//       document.getElementById("text_value").style.opacity = "0";
+//       window.setTimeout(() => {
+//         document.getElementById("text_value").innerHTML = speechArray[i];
+//         document.getElementById("text_value").style.opacity = "1";
+//       }, 1000);
+//     }
+//   }
+// };
 
+
+if(window.innerWidth<768){
+  const navLinks = document.querySelectorAll('.nav-item')
+  const menuToggle = document.getElementById('navbarNavAltMarkup')
+  const bsCollapse = new bootstrap.Collapse(menuToggle)
+  navLinks.forEach((l) => {
+      l.addEventListener('click', () => { bsCollapse.toggle() })
+  })
+}
